2008826f622a46236747a87527ceec01e93fa6fb
takemesnow <124555165+takemesnow@users.noreply.github.com>
2023-06-11 11:26:44 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
-SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
+UUID=${UUID:-'738635d1-61c3-4261-b673-3e24bc629a17'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/738635d1-61c3-4261-b673-3e24bc629a17-vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/738635d1-61c3-4261-b673-3e24bc629a17-vless'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/738635d1-61c3-4261-b673-3e24bc629a17-trojan'}
+SS_WSPATH=${SS_WSPATH:-'/738635d1-61c3-4261-b673-3e24bc629a17-shadowsocks'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
 sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
 sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
